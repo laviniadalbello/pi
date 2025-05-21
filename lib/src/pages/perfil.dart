b8c833e10a7f5db353b002c9f29f5b8fde30e344
Betania Pereira <betania.pereira@sou.unifeob.edu.br>
2025-05-21 20:05:29 -0300

    perfil - Lavinia

--- lib/src/pages/perfil.dart
+++ lib/src/pages/perfil.dart
@@ -1,8 +1,7 @@
+
 import 'package:flutter/material.dart';
 import 'dart:math';
 import 'configuracoes.dart';
-<<<<<<< HEAD
-=======
 import 'iconedaia.dart';
 
 const Color kDarkPrimaryBg = Color(0xFF1A1A2E);
@@ -13,7 +12,6 @@ const Color kAccentSecondary = Color(0xFF2CB67D);
 const Color kDarkTextPrimary = Color(0xFFFFFFFF);
 const Color kDarkTextSecondary = Color(0xFFA0AEC0);
 const Color kDarkBorder = Color(0xFF2D3748);
->>>>>>> 29e6bff (telasnovas)
 
 class PerfilPage extends StatefulWidget {
   const PerfilPage({super.key});
@@ -26,8 +24,6 @@ class _PerfilPageState extends State<PerfilPage> with TickerProviderStateMixin {
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
 
   late AnimationController _circleController;
-<<<<<<< HEAD
-=======
   late AnimationController _slideController;
   late Animation<Offset> _slideAnimation;
 
@@ -65,21 +61,15 @@ class _PerfilPageState extends State<PerfilPage> with TickerProviderStateMixin {
       "color": Colors.orangeAccent,
     },
   ];
->>>>>>> 29e6bff (telasnovas)
 
   @override
   void initState() {
     super.initState();
-<<<<<<< HEAD
-=======
 
->>>>>>> 29e6bff (telasnovas)
     _circleController = AnimationController(
       duration: const Duration(seconds: 6),
       vsync: this,
     )..repeat();
-<<<<<<< HEAD
-=======
 
     _slideController = AnimationController(
       duration: const Duration(milliseconds: 400),
@@ -89,51 +79,11 @@ class _PerfilPageState extends State<PerfilPage> with TickerProviderStateMixin {
       begin: const Offset(0, 1),
       end: Offset.zero,
     ).animate(CurvedAnimation(parent: _slideController, curve: Curves.easeOut));
->>>>>>> 29e6bff (telasnovas)
   }
 
   @override
   void dispose() {
     _circleController.dispose();
-<<<<<<< HEAD
-
-    super.dispose();
-  }
-
-  Widget _buildMenuCard(BuildContext context) {
-    double bottomPadding = 60.0;
-
-    return Padding(
-      padding: EdgeInsets.only(bottom: bottomPadding, left: 40, right: 40),
-      child: Material(
-        color: const Color.fromARGB(223, 17, 24, 39),
-        elevation: 8,
-        borderRadius: BorderRadius.circular(24),
-        child: Padding(
-          // Padding inside the card
-          padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 24),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              _menuItem(Icons.edit_outlined, 'Create Task'),
-              const SizedBox(height: 12),
-              _menuItem(Icons.add_circle_outline, 'Create Project'),
-              const SizedBox(height: 12),
-              _menuItem(Icons.group_outlined, 'Create Team'),
-              const SizedBox(height: 12),
-              _menuItem(Icons.schedule_outlined, 'Create Event'),
-              const SizedBox(height: 16),
-
-              FloatingActionButton(
-                mini: true,
-                backgroundColor: Colors.blueAccent,
-                elevation: 0,
-                shape: const CircleBorder(),
-                onPressed: () => Navigator.of(context).pop(), //
-                child: const Icon(Icons.close, size: 20, color: Colors.white),
-              ),
-            ],
-=======
     _slideController.dispose();
     super.dispose();
   }
@@ -262,15 +212,12 @@ class _PerfilPageState extends State<PerfilPage> with TickerProviderStateMixin {
                 ),
               ],
             ),
->>>>>>> 29e6bff (telasnovas)
           ),
         ),
       ),
     );
   }
 
-<<<<<<< HEAD
-=======
   Widget _menuItem(IconData icon, String label) {
     return Container(
       padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 16),
@@ -339,7 +286,6 @@ class _PerfilPageState extends State<PerfilPage> with TickerProviderStateMixin {
     );
   }
 
->>>>>>> 29e6bff (telasnovas)
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -352,10 +298,7 @@ class _PerfilPageState extends State<PerfilPage> with TickerProviderStateMixin {
       body: SafeArea(
         child: Stack(
           children: [
-<<<<<<< HEAD
-=======
             // Círculos animados (mantidos do original)
->>>>>>> 29e6bff (telasnovas)
             Positioned.fill(
               child: Stack(
                 children: [
@@ -391,91 +334,6 @@ class _PerfilPageState extends State<PerfilPage> with TickerProviderStateMixin {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-<<<<<<< HEAD
-                  Stack(
-                    alignment: Alignment.center,
-                    children: [
-                      Align(
-                        alignment: Alignment.centerLeft,
-                        child: GestureDetector(
-                          onTap: () {
-                            Navigator.pop(context);
-                          },
-                          child: const Icon(
-                            Icons.arrow_back,
-                            color: Colors.white,
-                          ),
-                        ),
-                      ),
-                      const Text(
-                        'Profile',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: 70),
-
-                  Column(
-                    children: [
-                      CircleAvatar(
-                        radius: 40,
-                        backgroundColor: Colors.white,
-                        child: Icon(
-                          Icons.image,
-                          color: Color(0xFF8875FF),
-                          size: 40,
-                        ),
-                      ),
-                      const SizedBox(height: 10),
-                      const Text(
-                        'USER',
-                        style: TextStyle(
-                          color: Color(0xFF8875FF),
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      const Text(
-                        '@nomedousuario',
-                        style: TextStyle(color: Colors.white70, fontSize: 12),
-                      ),
-                      const SizedBox(height: 8),
-                      ElevatedButton(
-                        onPressed: () {
-                          // ação do botão Edit
-                        },
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.transparent,
-                          foregroundColor: Colors.white,
-                          side: const BorderSide(
-                            color: Color.fromARGB(137, 130, 11, 241),
-                          ),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(8),
-                          ),
-                        ),
-                        child: const Text('Edit'),
-                      ),
-                    ],
-                  ),
-
-                  const SizedBox(height: 30),
-
-                  // Botão My Projects
-                  _fixedButton(text: 'My Projects'),
-
-                  const SizedBox(height: 20),
-
-                  // Botão Settings
-                  _fixedButton(text: 'Settings'),
-                ],
-              ),
-            ),
-          ],
-=======
                   // Cabeçalho
                   _buildHeader(),
                   const SizedBox(height: 30),
@@ -513,31 +371,11 @@ Positioned(
 ),
           ],
           
->>>>>>> 29e6bff (telasnovas)
         ),
       ),
     );
   }
 
-<<<<<<< HEAD
-  Widget _menuItem(IconData icon, String label) {
-    return AnimatedContainer(
-      duration: const Duration(milliseconds: 300),
-      curve: Curves.easeOut,
-      padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 16),
-      decoration: BoxDecoration(
-        border: Border.all(color: Colors.white10),
-        borderRadius: BorderRadius.circular(16),
-        color: Colors.white.withOpacity(0.03),
-      ),
-      child: Row(
-        children: [
-          Icon(icon, color: Colors.white, size: 20),
-          const SizedBox(width: 12),
-          Text(
-            label,
-            style: const TextStyle(color: Colors.white, fontSize: 14),
-=======
   Widget _buildHeader() {
     return Stack(
       alignment: Alignment.center,
@@ -785,75 +623,12 @@ Positioned(
                 fontWeight: FontWeight.bold,
               ),
             ),
->>>>>>> 29e6bff (telasnovas)
           ),
         ],
       ),
     );
   }
 
-<<<<<<< HEAD
-  Widget _buildFloatingActionButton() {
-    return Transform.translate(
-      offset: const Offset(0, 30),
-      child: FloatingActionButton(
-        backgroundColor: Colors.blueAccent,
-        elevation: 6,
-        shape: const CircleBorder(),
-        onPressed: () {
-          showModalBottomSheet(
-            context: context,
-            builder: _buildMenuCard,
-            backgroundColor: Colors.transparent,
-            elevation: 0,
-          );
-        },
-
-        child: const Icon(Icons.add, size: 28, color: Colors.white),
-      ),
-    );
-  }
-
-  Widget _buildBottomBar() {
-    return BottomAppBar(
-      color: Colors.black,
-      shape: const CircularNotchedRectangle(),
-      notchMargin: 8,
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Row(
-              children: [
-                IconButton(
-                  icon: const Icon(Icons.home_rounded),
-                  color: Colors.blueAccent,
-                  onPressed: () {},
-                ),
-                const SizedBox(width: 28),
-                IconButton(
-                  icon: const Icon(Icons.folder_rounded),
-                  color: Colors.white30,
-                  onPressed: () {},
-                ),
-              ],
-            ),
-            Row(
-              children: [
-                IconButton(
-                  icon: const Icon(Icons.chat_bubble_outline),
-                  color: Colors.white30,
-                  onPressed: () {},
-                ),
-                const SizedBox(width: 28),
-                IconButton(
-                  icon: const Icon(Icons.person_outline),
-                  color: Colors.white30,
-                  onPressed: () {},
-                ),
-              ],
-=======
   Widget _buildActionButtons() {
     return Column(
       children: [
@@ -942,7 +717,6 @@ Widget _buildAiCardSection(BuildContext context) {
               Icons.arrow_forward_ios,
               color: kDarkTextSecondary,
               size: 16,
->>>>>>> 29e6bff (telasnovas)
             ),
           ],
         ),
@@ -950,27 +724,6 @@ Widget _buildAiCardSection(BuildContext context) {
     );
   }
 
-<<<<<<< HEAD
-  Widget _fixedButton({required String text}) {
-    return Container(
-      height: 55,
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(12),
-      ),
-      child: ListTile(
-        title: Text(text, style: const TextStyle(color: Colors.black)),
-        trailing: const Icon(Icons.arrow_forward_ios, size: 16),
-        onTap: () {
-          if (text == 'Settings') {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => const SettingsApp()),
-            );
-          } else {}
-        },
-      ),
-=======
   void _showEditProfileDialog() {
     final nameController = TextEditingController(text: _userName);
     final handleController = TextEditingController(text: _userHandle);
@@ -1285,7 +1038,6 @@ Widget _buildAiCardSection(BuildContext context) {
               ),
             ],
           ),
->>>>>>> 29e6bff (telasnovas)
     );
   }
 
