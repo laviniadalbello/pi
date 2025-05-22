5142dcc111a1b949818e5c117e3ae13ce7ae7ccd
Luis <109842911+LuisGBF@users.noreply.github.com>
2025-05-21 22:20:27 -0300

    correçao na base, tasks, permissions e users

--- lib/services/firebase_permissions.dart
+++ lib/services/firebase_permissions.dart
@@ -1,12 +1,15 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class FirestorePermissions {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
   // Verifica se o usuario pode editar e adicionar as tasks ao projeto (nao mexer!!)
   Future<bool> canUserEdit(String projectId, String userId) async {
-    DocumentSnapshot doc = await _firestore
-        .collection('permissions')
-        .doc('${projectId}_$userId')
-        .get();
+    DocumentSnapshot doc =
+        await _firestore
+            .collection('permissions')
+            .doc('${projectId}_$userId')
+            .get();
     return doc.exists && doc['canEdit'] == true;
   }
 }
